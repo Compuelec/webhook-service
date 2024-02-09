e469ef6e0a9bbaeb2928885936671ad6b6ab7611
Carlos Abarca Vega <soporte@compuelec.cl>
2024-02-09 13:42:20 -0300

    Add CORS support to the application

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
   const port = parseInt(process.env.PORT, 10);
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
