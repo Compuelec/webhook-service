4f9f9365fa34f3368f78f67a01aa693463301c21
Carlos Abarca Vega <soporte@compuelec.cl>
2024-02-09 12:22:20 -0300

    Add dotenv package for environment variables

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3300;
-  console.log(`Server running on port ${port}`);
+  const port = parseInt(process.env.PORT, 10);
   await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
